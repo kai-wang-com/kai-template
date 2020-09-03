ea6a3622816104e3aedf9e0a0833fdc2acefdaae
kai.wang <99632463@qq.com>
2020-09-03 23:54:54 +0800

    merge

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    欢迎来到凯哥的手撕脚手架 ！！！
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  font-size:40px;
+  font-weight: bold;
+  color: red;
+}
+</style>
